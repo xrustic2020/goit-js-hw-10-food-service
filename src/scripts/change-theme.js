6ec059d068cb4727bde5e8be5957d6038a4074b7
Tisanyuk Ruslan <ruslantisanykot@gmail.com>
2020-12-03 17:36:56 +0200

    add default block in switch and del if else

--- src/scripts/change-theme.js
+++ src/scripts/change-theme.js
@@ -3,19 +3,20 @@ import Theme from './theme';
 import { save } from './storage';
 
 export default function () {
-  if (refs.body.hasAttribute('class')) {
-    switch (refs.body.getAttribute('class')) {
-      case Theme.DARK:
-        refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
-        save('theme', Theme.LIGHT);
-        break;
-      case Theme.LIGHT:
-        refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
-        save('theme', Theme.DARK);
-        break;
-    }
-  } else {
-    refs.body.classList.add(Theme.DARK);
-    save('theme', Theme.DARK);
-  }
+  switch (refs.body.getAttribute('class')) {
+    case Theme.DARK:
+      console.log('dark theme change for light');
+      refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
+      save('theme', Theme.LIGHT);
+      break;
+    case Theme.LIGHT:
+      console.log('light theme change for dark');
+      refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+      save('theme', Theme.DARK);
+      break;
+    default:
+      console.log('this default block theme');
+      refs.body.classList.add(Theme.DARK);
+      save('theme', Theme.DARK);
+  } 
 };
